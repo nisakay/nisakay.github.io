1f5b3ee00127e711e5db84d8c6ba18af97031e66
nisakay <nisakay@users.noreply.github.com>
2021-01-28 23:21:12 -0800

    Portfolio
    Added portfolio Section

--- JS/MyWebsite_Portfolio.js
+++ JS/MyWebsite_Portfolio.js
@@ -33,3 +33,6 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";        
 }
+
+var today = new Date();
+ document.getElementById('time').innerHTML=today;
